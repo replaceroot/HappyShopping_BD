f2d4a02208bf842fb62234a0ccd5912bc2a4c901
laiyu024@163.com <laiyu0425>
2019-10-30 16:41:22 +0800

    首页-分类导航部分搞定

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,22 +2,40 @@
  * @file index.js
  * @author swan
  */
-const app = getApp()
+const app = getApp();
 import { request } from "../../request/index.js";
 Page({
-    data: {
-        // 轮播图数据列表
-        swiperList: []
-    },
-    onLoad() {
-        request({
-            url: '/home/swiperdata'
-        }).then(res => {
-            console.log(res);
-            let swiperList = res;
-            this.setData({
-                swiperList
-            })
-        })
-    }
-})
+  data: {
+    // 轮播图数据列表
+    swiperList: [],
+    // 分类导航数据列表
+    navCateList: []
+  },
+  onLoad() {
+    this.getSwiperList();
+    this.getNavCateList();
+  },
+  // 获取轮播图数据方法
+  getSwiperList() {
+    request({
+      url: "/home/swiperdata"
+    }).then(res => {
+      let swiperList = res;
+      this.setData({
+        swiperList
+      });
+    });
+  },
+
+  // 获取分类导航数据方式
+  getNavCateList() {
+    request({
+      url: "/home/catitems"
+    }).then(res => {
+      let navCateList = res;
+      this.setData({
+        navCateList
+      });
+    });
+  }
+});
