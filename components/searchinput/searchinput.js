e7665ce3b6abed47823b005c2f934d982c1a4d48
laiyu024@163.com <laiyu0425>
2019-10-30 15:26:13 +0800

    首页-搜索组件搞定

---
+++ components/searchinput/searchinput.js
@@ -0,0 +1,17 @@
+Component({
+    properties: {
+        propName: { // 属性名
+            type: String, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
+            value: 'val', // 属性初始值（必填）
+            observer: function(newVal, oldVal) {
+                // 属性被改变时执行的函数（可选）
+            }
+        }
+    },
+
+    data: {}, 
+
+    methods: {
+        
+    }
+});
